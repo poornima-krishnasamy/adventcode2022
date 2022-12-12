a6e456794ced95ada842290a3723455411ed6c59
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2022-12-12 17:51:12 +0000

    Part 2 - day 3

--- day3/main.go
+++ day3/main.go
@@ -17,26 +17,37 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	total := 0
+	lineNum := 1
+	grp := make(map[int]struct{})
+	common := make(map[int]struct{})
 	for scanner.Scan() {
 		lineStr := scanner.Text()
-		len := len(lineStr) / 2
-		var priority byte
-	NEXT:
-		for i := 0; i < len; i++ {
-			for j := len; j < len*2; j++ {
-				if lineStr[i] == lineStr[j] {
-					priority = lineStr[i]
-					if priority >= 97 {
-						total += ((int(priority) - 97) + 1)
-					} else {
-						total += (26 + (int(priority) - 65) + 1)
-					}
-					break NEXT
-				}
+
+		for i := range lineStr {
+			priority := int(lineStr[i])
+			if priority >= 65 && priority < 91 {
+				priority = priority - 65 + 27
+			} else if priority >= 97 && priority < 123 {
+				priority = priority - 97 + 1
 			}
 
+			if lineNum%3 == 0 {
+				if _, ok := common[priority]; ok {
+					total += priority
+					// reset for next round of 3 lines
+					grp = make(map[int]struct{})
+					common = make(map[int]struct{})
+					break
+				}
+			} else if lineNum%3 == 2 {
+				if _, ok := grp[priority]; ok {
+					common[priority] = struct{}{}
+				}
+			} else if lineNum%3 == 1 {
+				grp[priority] = struct{}{}
+			}
 		}
-
+		lineNum += 1
 	}
 	fmt.Println(total)
 }
